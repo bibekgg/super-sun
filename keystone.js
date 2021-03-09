43ac11d852ece8816ebd08d90174a02ad3b4466e
Bibek Gurung <bibek.gurung95@gmail.com>
2021-03-09 23:16:15 +0545

    upgrade package

--- keystone.js
+++ keystone.js
@@ -28,6 +28,10 @@ keystone.init({
 		defaultLayout: 'default',
 		helpers: new require('./templates/views/helpers')(),
 		extname: '.hbs',
+		runtimeOptions: {
+			allowProtoPropertiesByDefault: true,
+			allowProtoMethodsByDefault: true
+		}
 	}).engine,
 	'auto update': true,
 	'session': true,
